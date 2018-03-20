1dcd8536d717c51d5eb81fceac19f7540999232b
Venkata Sairam <venkatasairam.lanka@gmail.com>
2018-03-20 12:33:10 +0530

    react-redux poc explaining components,containers,actions,reducers,state and props(Venkata Sairam)

---
+++ src/app/containers/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import  {connect} from  "react-redux";
+import { User } from '../components/User';
+import { Main } from '../components/Main';
+import { setName } from "../actions/userAction";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <Main changeUsername={() => this.props.setName("VENKATA SAIRAM LANKA")}/>
+                <User username={this.props.user.name}/>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) =>{
+    return {
+        user:state.user,
+        math:state.math
+    }
+}
+
+const mapDispatchToProps = (dispatch) =>{
+    return {
+        setName: (name) => {
+            dispatch(setName(name));
+        }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
